97ae653fd12a118e7eb952a20a5127895c06c70f
Gopimopidevi3p5 <ravaagopi3p5@gmail.com>
2026-01-21 11:55:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const  form = document.querySelector("form")
+const existing = document.getElementById("existing")
+if (localStorage.getItem("username")) {
+    existing.style.display = "block";
+  }
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const user = e.target.username.value;
+    const pass = e.target.password.value;
+	const checkbox = e.target.checkbox
+
+    alert(`Logged in as ${user}`);
+	
+
+    if (checkbox.checked) {
+      localStorage.setItem("username", user);
+      localStorage.setItem("password", pass);
+      existing.style.display = "block";
+    } else {
+      localStorage.clear();
+      existing.style.display = "none";
+    }
+  });
+
+  existing.addEventListener("click", function () {
+    alert(`Logged in as ${localStorage.getItem("username")}`);
+  });
